121d46c2124da7ecbcc06f6d03119a208cbb702e
kheman1604 <official.kheman@gmail.com>
2021-02-01 20:43:56 +0530

    hhbh

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,11 +1,13 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {
+  createProxyMiddleware
+} = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'http://localhost:3003',
-        changeOrigin: true,
-      })
-    );
-  };
+module.exports = function (app) {
+  app.use(
+    '/api/',
+    createProxyMiddleware({
+      target: 'http://localhost:3003',
+      changeOrigin: true,
+    })
+  );
+};
